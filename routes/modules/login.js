b8fd4fc7bd7b1659eeb61f5a8beb1d365bfa578a
Banwind <a84012807@gmail.com>
2023-03-14 21:29:24 +0800

    feat: modify login.js

--- routes/modules/login.js
+++ routes/modules/login.js
@@ -3,12 +3,17 @@ const router = express.Router()
 const userModel = require('../../models/userModel')
 
 router.post('/', (req, res) => {
-
+  const { email, password } = req.body
   userModel.findOne({ email })
     .lean()
-    .then((doc) => 
-      if ()
-    )
+    .then((doc) => {
+      if (doc.password === password) {
+        res.redirect('/home')
+      } else {
+        alert('Wrong password')
+      }
+    })
+    .catch(error => console.log(error))
 })
 
 module.exports = router
