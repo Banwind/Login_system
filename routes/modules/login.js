3ca792ea435307a5a60c77f4b4bc6e31b86f8e2e
Banwind <a84012807@gmail.com>
2023-03-14 20:33:51 +0800

    feat: modify router login.js

--- routes/modules/login.js
+++ routes/modules/login.js
@@ -3,7 +3,12 @@ const router = express.Router()
 const userModel = require('../../models/usermodel')
 
 router.post('/', (req, res) => {
+  
   userModel.findOne({ email })
+    .lean()
+    .then((doc) => 
+      if ()
+    )
 })
 
 module.exports = router
