79aada5dce9f30129d7bdef314b6bdf45d224c73
Banwind <a84012807@gmail.com>
2023-03-14 20:15:09 +0800

    feat: router index.js

--- routes/index.js
+++ routes/index.js
@@ -2,3 +2,8 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const login = require('./modules/login')
+
+router.use('/home', home) //登入後第一個頁面
+router.use('/', login) //登入畫面
+
+module.exports = router
