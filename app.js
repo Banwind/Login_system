d91e8d3133ed2887220b08c286c80a2a2f11b75b
Banwind <a84012807@gmail.com>
2023-03-14 20:11:35 +0800

    init

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const methodOverride = require('method-override')
+const routes = require('./routes')
+const PORT = 3000
+
+require('./config/mongoose')
+
+app.engine('hbs', exphbs({defaultLayouts: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`This is running on localhost:${PORT}`)
+})
---
+++ routes/index.js
@@ -0,0 +1,4 @@
+const express = require('express')
+const router = express.Router()
+const home = require('./modules/home')
+const login = require('./modules/login')
